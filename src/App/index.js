997ae6b83fe75ef40072c487e384d0b9d01e4ff8
juandiegocr01 <juandiegocr2009@hotmail.com>
2023-01-09 09:54:18 -0500

    Organización de carpetas

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,6 @@
 //import './App.css';
 import React from "react";
-import {TodoCounter} from './TodoCounter';
-import {TodoSearch} from './TodoSearch';
-import { TodoItem } from './TodoItem';
-import { TodoList } from "./TodoList";
-import { CreateTodoButton } from "./CreateTodoButton";
+import { AppUI } from "./AppUI";
 
 const defaultTodos = [
   { text: 'Cortar cebolla', completed: false },
@@ -51,32 +47,15 @@ function App() {
  
 
   return (
-    <React.Fragment>
-      <TodoCounter 
-      total={totalTodos}
-      completed={completedTodos}
-      />
-      <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onDelete={() => deleteTodo(todo.text)}
-            onComplete={() => completeTodo(todo.text)}
-            
-          />
-          
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-    </React.Fragment>
+   <AppUI
+   totalTodos={totalTodos}
+   completedTodos={completedTodos}
+   searchValue={searchValue}
+   setSearchValue={setSearchValue}
+   searchedTodos={searchedTodos}
+   completeTodo={completeTodo}
+   deleteTodo={deleteTodo}
+   />
   );
 }
 
